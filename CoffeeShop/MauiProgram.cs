6aa75293c354c64a955ab78519005bd62e522d60
Ogbonnah Kingsley <kingsleyogbonna0@gmail.com>
2023-10-27 14:00:32 +0300

    initial commit

---
+++ CoffeeShop/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CoffeeShop
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("fontello.ttf", "Icons");
+                    fonts.AddFont("Rubik-Italic-VariableFont_wght.ttf", "RubikLight");
+                    fonts.AddFont("Rubik-VariableFont_wght.ttf", "RubikRegular");
+                });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
